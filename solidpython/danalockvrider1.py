1f0d62da5a16bc430f4c735de6d0563826b134d5
Torsten Martinsen <torsten@bullestock.net>
2021-04-10 12:13:06 +0200

    Make a little more room after the insert.

--- solidpython/danalockvrider1.py
+++ solidpython/danalockvrider1.py
@@ -23,11 +23,11 @@ def assembly():
     p1 = cyl1 - trans(0, -(5 + wi/2), -1, cut) - trans(0, (5 + wi/2), -1, cut)
     disc = cylinder(d = 25, h = disc_th)
     center_hole = cylinder(d = 3.5, h = free_h + disc_th + 2)
-    insert_hole = cylinder(d = 4.2, h = 5)
+    insert_hole = cylinder(d = 4.2, h = 6)
     mount_hole = cylinder(d = 3.5, h = 5) + cylinder(d = 7, h = 1)
     d = 15
     mount_holes = trans(-d/2, 0, 0, mount_hole) + trans(d/2, 0, 0, mount_hole)
-    return p1 + up(free_h)(disc) - down(1)(center_hole) - up(free_h - 1.5)(insert_hole) - up(free_h - e)(mount_holes)
+    return p1 + up(free_h)(disc) - down(1)(center_hole) - up(free_h - 2.5)(insert_hole) - up(free_h - e)(mount_holes)
 
 if __name__ == '__main__':
     a = assembly()
