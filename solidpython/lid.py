7789f53fa0a28d7b31221be5eac8b3e09c619e75
Torsten Martinsen <torsten@bullestock.net>
2021-05-30 18:10:17 +0200

    Finalize Wemos holder.

--- solidpython/lid.py
+++ solidpython/lid.py
@@ -37,7 +37,7 @@ filler_offset = 10
 
 led_y = -5
 
-def pod():
+def wemos_holder():
     pod_l = 15
     pod_extra_w = 5
     pod_h = 12
@@ -47,13 +47,42 @@ def pod():
     usb_h = 8
     usb_hole = ccube(pod_l + 5, usb_w, usb_h)
     pod = trans(-(wm_l + 2*wt + pod_l)/2, 0, -pod_h/2, down(1)(pod_outer) - trans(1, wm_usb_offset, (pod_h - usb_h)/2, hole()(usb_hole)))
-    plughole = trans(dia/2, led_y, 10, ccube(5, 4.7, 6))
-    plugtube = trans(dia/2 - 4, led_y, 10-1.5, ccube(5, 4.7+3, 6+3))
-    es = .5
+
+    # Hole for power plug (JST PH)
+    pwr_plug_l = 5
+    pwr_plug_x = -(wm_l + pod_l)/2 - pwr_plug_l - 1
+    pwr_plug_y = 11
+    pwr_plug_z = -4
+    es = .5 # empirics
+    pwr_plug_w = 4.7 + es
+    pwr_plug_h = 6 + es
+    pwr_wire_sz = 4
+    pwr_wirehole = trans(pwr_plug_x + 4, pwr_plug_y, pwr_plug_z + (pwr_plug_h - pwr_wire_sz)/2, ccube(pod_l + 3, pwr_wire_sz, pwr_wire_sz))
+    pwr_plughole = trans(pwr_plug_x - 1, pwr_plug_y, pwr_plug_z, ccube(pwr_plug_l + 1, pwr_plug_w, pwr_plug_h))
+    pwr_plugtube = trans(pwr_plug_x, pwr_plug_y, pwr_plug_z-1.5, ccube(pwr_plug_l, pwr_plug_w+3, pwr_plug_h+3))
+
+    # Hole for door switch (2.54 mm header)
+    sw_plug_x = -(wm_l + 2*wt + pod_l - 2)/2
+    sw_plug_y = -11
+    sw_plug_l = pod_l - 2
+    sw_plug_z = -4
+    es = 0 # empirics
+    sw_hdr_w = 2.5 + es
+    sw_hdr_h = 5.02 + es
+    sw_housing_w = 1+2.6 + es
+    sw_housing_h = 1+2*2.6 + es
+    sw_housing_l = 10
+    sw_hdrhole = trans(sw_plug_x - 1, sw_plug_y, sw_plug_z, ccube(sw_plug_l + 10, sw_hdr_w, sw_hdr_h))
+    sw_housinghole = trans(sw_plug_x - (sw_plug_l - sw_housing_l) - 1, sw_plug_y, sw_plug_z - (sw_housing_h - sw_hdr_h)/2, ccube(sw_housing_l, sw_housing_w, sw_housing_h))
+    sw_hdrtube = trans(sw_plug_x, sw_plug_y, sw_plug_z-1.5, ccube(sw_plug_l, sw_hdr_w+3, sw_hdr_h+3))
+
+    es = .1 # empirics
     frame_inner = ccube(wm_l+es, wm_w+es, wm_th+e)
-    frame_outer = ccube(wm_l + 2*wt, wm_w + 2*wt, 2*wm_th)
+    frame_outer = roundccube(wm_l + 2*wt, wm_w + 2*wt, 2*wm_th, 1)
     frame_cut = trans(-wm_l/2, wm_usb_offset, -wm_th, ccube(5, 12, wm_th+2))
-    return pod + frame_outer - frame_inner - frame_cut #pod_outer - pod_inner + plugtube - plughole + 
+    return pod + trans(-1, 0, 0, frame_outer - frame_inner - frame_cut) \
+        - sw_hdrhole - sw_housinghole - pwr_wirehole - pwr_plughole
+
 
 def smps():
     w = 17
@@ -88,7 +117,7 @@ def shell():
     return outer - inner - cutter - rounder1 - rounder2 - slit1 - slit2 + filler - screwhole + ledtube - ledhole
 
 def assembly():
-    return pod() #shell() + smps() #
+    return wemos_holder() #shell() + smps() #
 
 if __name__ == '__main__':
     a = assembly()
