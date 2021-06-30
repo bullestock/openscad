144f786a4c0e3f8e3eea03aa119795b11c3106a4
Torsten Martinsen <torsten@bullestock.net>
2021-06-30 22:24:21 +0200

    Make stuff out of spheres.

--- solidpython/lasersaurdisplay.py
+++ solidpython/lasersaurdisplay.py
@@ -4,6 +4,7 @@ from __future__ import division
 import os
 import sys
 import re
+import math
 
 # Assumes SolidPython is in site-packages or elsewhwere in sys.path
 from solid import *
@@ -12,7 +13,7 @@ from utils import *
 
 esp = import_scad('ESP8266Models.scad')
 
-SEGMENTS = 32
+SEGMENTS = 16#32
 
 e = 0.001
 
@@ -36,14 +37,33 @@ def oled(x, y, z, a, negative = False):
                                        trans(0, 15.5, 4, ccube(10, 2.5, 10)) +
                                        trans(0, 2, -4 if negative else -1, ccube(32, 18, 4 + extra)))))
 
-def front(x, y, z, a, print = True):
-    c = trans(0, 0, -cr, roundccube(width, disp_height, cr/4, cr));
+def front_cuts(x, y, z, a, print = True):
+    c = None
     if not print:
-        c = c - trans(0, 2, -2, ccube(32, 18, 2*cr+2))
-    f = trans(x, y, z,
-              rot(90 + a, 0, 0, c))                  
-    return f
-                     
+        c = trans(0, 2, -2, ccube(32, 18, 2*cr+2))
+        c = trans(x, y, z,
+                  rot(90 + a, 0, 0, c))                  
+    return c
+
+def front_s(x, y, z, a):
+    #def roundcube_s(length, width, height, radius):
+    #return translate([length/2, width/2, 0])(roundccube_s(length, width, height, radius))
+    ss = roundccube_s(width, disp_height, 2*cr+1, cr)
+    for i in range(0, 8):
+        c = trans(0, 0, -cr, ss[i])
+        f = trans(x, y, z,
+                  rot(90 + a, 0, 0, c))
+        ss[i] = f
+    return ss
+
+def top_s(x, y, z, depth):
+    return trans(x, y + depth/2, z, roundccube_s(width, depth, 2*cr+1, cr))
+    
+def back_s(x, y, z, height):
+    ss = roundccube_s(width, 2*cr+1, height, cr)
+    for i in range(0, 8):
+        ss[i] = trans(x, y, z, ss[i])
+    return ss
     
 def assembly():
     print = True  # Print
@@ -52,7 +72,19 @@ def assembly():
     disp_y = 12
     disp_z = 3
     o = oled(0, disp_y, disp_z, tilt, print)
-    f = front(0, disp_y - 1, disp_z, tilt, print)
+    #f = front_s(
+    f_s = front_s(0, disp_y - 1, disp_z, tilt)
+    f = hull()(f_s) - front_cuts(0, disp_y - 1, disp_z, tilt, print)
+    top_depth = 25
+    top_y = disp_y - top_depth - disp_height/2*math.sin(90 + tilt)
+    top_z = disp_z + disp_height/2*math.cos(90+tilt)
+    t_s = top_s(0, top_y + 1.5*cr, top_z - 1.5*cr, top_depth)
+    t = hull()(t_s)
+    back_h = disp_height*math.cos(90+tilt)
+    b_s = back_s(0, top_y + 2.8*cr, top_z - back_h, back_h)
+    b = hull()(b_s)
+    side1 = hull()(f_s[0] + f_s[6] + b_s[0] + b_s[4])
+    side2 = hull()(f_s[1] + f_s[7] + b_s[1] + b_s[5])
     # Visualization
     if not print:
         a = w + f + o
@@ -62,6 +94,8 @@ def assembly():
     #a = w + f - o
     # Test of OLED fit
     #a = f - o
+    a = a + t + b + side1 + side2
+    #a = side2
     return a
 
 if __name__ == '__main__':
