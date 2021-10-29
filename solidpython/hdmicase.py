222345ae714af7c1fb71d549551bd6cb2772ba0b
Torsten Martinsen <torsten@bullestock.net>
2021-10-29 18:41:21 +0200

    Finish lower part.

--- solidpython/hdmicase.py
+++ solidpython/hdmicase.py
@@ -15,7 +15,7 @@ SEGMENTS = 64
 eps = 0.001
 
 pcb_w = 18
-pcb_l = 28.75
+pcb_l = 25
 pcb_h = 3.6
 
 vga_w = 18
@@ -26,13 +26,15 @@ hdmi_w = 14.5
 hdmi_l = 1.1
 hdmi_h = 5
 
+extra = 2
+
 # shell thickness
 sh = 1.5
 
-def assembly():
-    pcb = yccube(pcb_w, pcb_l, pcb_h)
+def lower():
+    pcb = yccube(pcb_w, pcb_l, pcb_h + extra)
     vga = yccube(vga_w, vga_l + 1, vga_h)
-    hdmi = yccube(hdmi_w, hdmi_l + 1, hdmi_h)
+    hdmi = yccube(hdmi_w, hdmi_l + 1, hdmi_h + extra)
     hole = trans(0, -1 + eps, 0, vga) + trans(0, vga_l, 2, pcb) + trans(0, vga_l + pcb_l - eps, 1, hdmi)
     o_l = vga_l + pcb_l + hdmi_l
     o_h = vga_h + 2*sh
@@ -40,8 +42,11 @@ def assembly():
     cutter_h = 20
     cutter_w = pcb_w * 2
     cutter = trans(-cutter_w/2, -5, 0, cube([cutter_w, o_l + 10, cutter_h]))
-    cut_z = 5
+    cut_z = 7
     return trans(0, o_l/2, -sh, outer) - hole - up(cut_z)(cutter)
+
+def assembly():
+    return lower()
 
 if __name__ == '__main__':
     a = assembly()
