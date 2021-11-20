e1feda33f6e1e103c813483dc31200ac191a1558
Torsten Martinsen <torsten@bullestock.net>
2021-11-20 14:12:18 +0100

    Add bumps.

--- solidpython/esp32camtop.py
+++ solidpython/esp32camtop.py
@@ -4,13 +4,14 @@ from __future__ import division
 import os
 import sys
 import re
+import math
 
 # Assumes SolidPython is in site-packages or elsewhwere in sys.path
 from solid import *
 from solid.utils import *
 from utils import *
 
-SEGMENTS = 64#128
+SEGMENTS = 32
 
 eps = 0.01
 rr = 2
@@ -20,7 +21,7 @@ a = 28
 b = a + 4*wth
 c = 40.5
 d = c + 4*wth
-gap = 0.2
+gap = 0.3
 # Height of pinheaders above rim
 h = 14
 # Dimple
@@ -29,11 +30,22 @@ dd2 = 6.75
 dh = 1.5
 
 def dimple(a):
-    pad = roundccube(dd1, 2, dd1 + 2, 1)
+    pad = roundccube(dd1, 3, dd1 + 2, 1)
     offset = wth
     if a > 0:
         offset = -offset
-    return hole()(rot(a, 90, 90, cylinder(d1 = dd1, d2 = dd2, h = dh))) + trans(0, offset, -dd1/2, pad)
+    part = hole()(rot(a, 90, 90, cylinder(d1 = dd1, d2 = dd2, h = dh))) + trans(0, offset, -dd1/2, pad)
+    r = 10
+    offset = -0.3
+    for angle in range(-2, 3):
+        d1 = trans(r * math.cos((angle + offset)/math.pi), 0, r * math.sin((angle + offset)/math.pi), dot())
+        part = part + d1
+        d1 = trans(-r * math.cos((angle - offset)/math.pi), 0, -r * math.sin((angle - offset)/math.pi), dot())
+        part = part + d1
+    return part
+
+def dot():
+    return sphere(r = 1.2)
 
 def wirehole():
     d = 2.5
@@ -43,9 +55,11 @@ def wirehole():
 
 def assembly():
     base = roundxycube(d, b, h + wth, rr)
-    cutout = roundxycube(c + 2*wth + gap, a + 2*wth + gap, h, rr/2)
+    cutout1 = roundxycube(c + 2*wth + gap, a + 2*wth + gap, 6, rr/2)
+    cutout2 = roundxycube(c, a, h, rr/2)
+    print("b: %d" % b)
     dimples = trans(0, b/2 + eps, (h + 2*wth)/2, dimple(180)) + trans(0, -b/2 - eps, (h + 2*wth)/2, dimple(0))
-    return base - down(eps)(cutout) + dimples - trans(d/2 - 2*wth, 0, h - 3, wirehole())
+    return base - down(eps)(cutout1 + cutout2) + dimples - trans(d/2 - 2*wth, 0, h - 3, wirehole())
 
 if __name__ == '__main__':
     a = assembly()
