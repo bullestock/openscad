e6eec4a82079b2845060a833708797b512038d50
Torsten Martinsen <torsten@bullestock.net>
2021-12-07 18:18:26 +0100

    Adjust dimensions.

--- solidpython/razortop.py
+++ solidpython/razortop.py
@@ -11,36 +11,46 @@ from solid import *
 from solid.utils import *
 from utils import *
 
-SEGMENTS = 32#64#128
+SEGMENTS = 64#128
 
 eps = 0.001
 rr = 2
 
 # Brush
-bhd = 30
+bhd = 34
 
 # Razor
-rhd = 10
+rhd = 12
+
+bridge_l = 15
 
 def sph(dia, radius, angle):
     rads = angle/360*2*math.pi
-    return trans((dia+2*radius)*math.cos(rads), (dia+2*radius)*math.sin(rads), 0, sphere(r = radius))
+    return trans((dia/2+radius)*math.cos(rads), (dia/2+radius)*math.sin(rads), 0, sphere(r = radius))
 
 def fork(dia, th, angle):
     s1 = sph(dia, th/2, angle)
     s2 = sph(dia, th/2, 0)
-    return rot(0, 0, (360-angle)/2, s1 + s2 + rotate_extrude(angle=angle)(translate([dia+th, 0])(circle(d=th))))
+    return rot(0, 0, (360-angle)/2, s1 + s2 + rotate_extrude(angle=angle)(translate([dia/2+th/2, 0])(circle(d=th))))
 
-def assembly():
-    bridge_l = 15
+def stand():
     bridge_w = 10
     fork_th = 5
-    bfork = trans(bhd + fork_th + bridge_l/2, 0, 0, fork(bhd, fork_th, 250))
-    rfork = trans(-(rhd + fork_th + bridge_l/2), 0, 0, rot(0, 0, 180, fork(rhd, fork_th, 250)))
+    bfork = trans(bhd/2 + fork_th/2 + bridge_l/2, 0, 0, fork(bhd, fork_th, 240))
+    rfork = trans(-(rhd/2 + fork_th/4 + bridge_l/2 - 0.5), 0, 0, rot(0, 0, 180, fork(rhd, fork_th, 245)))
     c = trans(-bridge_l/2, 0, 0, rot(90, 0, 90, cylinder(d = fork_th, h = bridge_l)))
     bridge = hull()(trans(0, -bridge_w/2, 0, c) + trans(0, bridge_w/2, 0, c))
     hole = down(fork_th)(cylinder(d=5, h=3*fork_th))
     return bfork + rfork + bridge - hole
+
+def brush():
+    return color(c = 'white')(down(10)(cylinder(d = 30, h = 10) + up(10-eps)(cylinder(d1 = 30, d2 = 38, h = 20))))
+
+def razor():
+    return color(c = 'silver')(down(5)(cylinder(d = 9, h = 10) + down(15-eps)(cylinder(d1 = 14, d2 = 9, h = 20))))
+
+def assembly():
+    return stand() #+ trans(bhd/2 + bridge_l/2, 0, 0, brush()) + trans(-(rhd/2 + bridge_l/2), 0, 0, razor())
 
 if __name__ == '__main__':
     a = assembly()
