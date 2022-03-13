b64e7519eef11a5c662f74705710d3067c8ec665
Torsten Martinsen <torsten@bullestock.net>
2022-03-13 12:09:28 +0100

    Use a tactile switch.

--- solidpython/frontdoorswitch.py
+++ solidpython/frontdoorswitch.py
@@ -13,45 +13,52 @@ from utils import *
 SEGMENTS = 16#64
 
 eps = 0.01
-sw_w = 12
-sw_h = 5
-sw_th = 5.6
+width = 12
+sw_h = 3
+sw_w = 6
 gap = 0.9
 lever_h = 2.5
 extra_h = gap + lever_h
-plate_th = 1.5 # 2.7
+plate_th = 1.3
 holes_dist = 6.35
 hole_inset = 2
+rod_dia = 1.4
 
 def block():
-    return cube([sw_w, sw_h + extra_h + lever_h/2, plate_th])
+    return cube([width, sw_h + extra_h + lever_h/2, plate_th])
 
 def cyl():
-    # actual diameter 1.85
-    return cylinder(d = 1.8, h = sw_th/2 - 0.1)
+    return 
 
 def part(flip):
-    c = cyl()
-    dy = 1.6
+    c = cylinder(d = rod_dia, h = plate_th + 2)
+    horiz_gap = 1
     return block() + \
-        trans(sw_w/2 - holes_dist/2, dy, plate_th - eps, c) + \
-        trans(sw_w/2 + holes_dist/2, dy, plate_th - eps, c) + \
-        trans(sw_w - hole_inset if flip else hole_inset,
-              sw_h + extra_h, plate_th - eps, c)
+        trans(width - sw_w + horiz_gap if flip else 0,
+              0,
+              plate_th - eps,
+              cube([width - sw_w - horiz_gap, sw_h, sw_w])) + \
+        trans(0 if flip else width - sw_w,
+              0,
+              plate_th - eps,
+              cube([sw_w, 1, sw_w])) - \
+        trans(width - hole_inset if flip else hole_inset,
+              sw_h + extra_h, -1, c)
 
 def lever():
-    return cube([sw_w, lever_h, sw_th]) - \
-        trans(hole_inset, lever_h/2, -1, cylinder(d = 1.9, h = sw_th + 2))
+    return cube([width, lever_h, sw_w]) - \
+        trans(hole_inset, lever_h/2, -1, cylinder(d = rod_dia, h = sw_w + 2))
     
 def assembly():
-    return part(False) + trans(sw_w+1, 0, 0, part(True)) + \
+    return part(False) + trans(width+1, 0, 0, part(True)) + \
         trans(0, sw_h + extra_h + lever_h, 0, lever())
 
 if __name__ == '__main__':
-    #a = assembly()
+    a = assembly()
     p1 = part(False)
     p2 = part(True)
     p3 = lever()
+    scad_render_to_file(a, 'frontdoorswitch.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
     scad_render_to_file(p1, 'part1.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
     scad_render_to_file(p2, 'part2.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
     scad_render_to_file(p3, 'part3.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
