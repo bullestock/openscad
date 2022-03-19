330a76b35b09bd131661886a7cced32a8ea0c9b3
Torsten Martinsen <torsten@bullestock.net>
2022-03-19 12:12:14 +0100

    Tweak some more.

--- solidpython/frontdoorswitch.py
+++ solidpython/frontdoorswitch.py
@@ -18,9 +18,9 @@ sw_h = 3
 sw_w = 6
 button_h = 1.35
 lever_h = 2.5
-sw_plate_th = 1.5
+sw_plate_th = 0.5
 extra_h = sw_plate_th + button_h + lever_h
-plate_th = 1.3
+plate_th = 1.5
 holes_dist = 6.35
 hole_inset = 2
 rod_dia = 1.4
@@ -53,12 +53,14 @@ def part2():
               sw_plate_th + sw_h + button_h + lever_h/2, -1, c)
 
 def lever():
-    return cube([width, lever_h, sw_w]) - \
+    clearance = 0.5
+    return cube([hole_inset*2, lever_h, sw_w]) + \
+        trans(0, 0, clearance, cube([width, lever_h, sw_w - 2*clearance])) - \
         trans(hole_inset, lever_h/2, -1, cylinder(d = rod_dia, h = sw_w + 2))
     
 def assembly():
     return part1() + trans(width+1, 0, 0, part2()) + \
-        trans(0, sw_h + extra_h + lever_h, 0, lever())
+        trans(0, sw_h + extra_h + lever_h + sw_w, 0, rot(90, 0, 0, lever()))
 
 if __name__ == '__main__':
     a = assembly()
