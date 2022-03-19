7bd4f2da5acda088516dabb922219ebd612f058e
Torsten Martinsen <torsten@bullestock.net>
2022-03-19 21:34:03 +0100

    Improve lever.

--- solidpython/frontdoorswitch.py
+++ solidpython/frontdoorswitch.py
@@ -15,12 +15,12 @@ SEGMENTS = 64
 eps = 0.01
 width = 12
 sw_h = 3
-sw_w = 6.5
+sw_w = 6.3
 button_h = 1.35
 lever_h = 2.5
 sw_plate_th = 0.5
 extra_h = sw_plate_th + button_h + lever_h
-plate_th = 1.25
+plate_th = 1.35
 holes_dist = 6.35
 hole_inset = 2
 rod_dia = 1.4
@@ -59,12 +59,16 @@ def part2():
               -eps, cube([width - 2 * hole_inset + eps, lever_h, 5]))
 
 def lever():
-    stopper = cube([hole_inset, button_h, sw_w])
-    clearance = 0.25
-    return cube([hole_inset*2 + clearance, lever_h, sw_w]) + \
-        trans(2 * hole_inset + clearance, 0, -plate_th, cube([width - 2 * hole_inset, lever_h, sw_w + 2 * plate_th])) - \
-        trans(hole_inset, lever_h/2, -1, cylinder(d = rod_dia, h = sw_w + 2)) + \
-        trans(0, lever_h - eps, 0, stopper)
+    clearance = 0.5
+    stopper = cube([hole_inset, button_h - 0.1, sw_w - 2*clearance])
+    pivot = cube([hole_inset*2 + clearance, lever_h, sw_w]) - \
+        trans(hole_inset, lever_h/2, -1, cylinder(d = rod_dia, h = sw_w + 2))
+    upper_body = trans(2 * hole_inset + clearance, 0, -plate_th, cube([width - 2 * hole_inset, eps, sw_w + 2 * plate_th]))
+    inset = 1
+    lower_body = trans(2 * hole_inset + clearance, 0, inset,
+                       cube([width - 2 * hole_inset, eps, sw_w - 2*inset]))
+    body = hull()(upper_body + trans(0, lever_h, 0, lower_body))
+    return pivot + trans(0, lever_h - eps, clearance, stopper) + body
     
 def assembly():
     return part1() + trans(width+1, 0, 0, part2()) + \
@@ -78,7 +82,7 @@ if __name__ == '__main__':
     scad_render_to_file(a, 'frontdoorswitch.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
     scad_render_to_file(p1, 'part1.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
     scad_render_to_file(p2, 'part2.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
-    scad_render_to_file(p3, 'part3.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
+    scad_render_to_file(p3, 'lever.scad', file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
 
 
 # Local Variables:
